368c00fa7a9fb165b9dc40b25d4689fd2338fda7
dengshilong <dengshilong1988@gmail.com>
2017-02-06 17:53:05 +0800

    fix: add requirements.txt

--- ydd.py
+++ ydd.py
@@ -2,16 +2,15 @@ import requests
 import json
 import click
 API_URL = "http://fanyi.youdao.com/openapi.do?keyfrom=ydd-dict&key=1890264650&type=data&doctype=json&version=1.1"
-
 s = """{"basic": {"explains": ["[试验] test", "measurement"], "phonetic": "cè shì"}, "query": "测试", "web": [{"key": "测试",
 "value": ["Test", "test", "TST test"]}, {"key": "集成测试", "value": ["Integration testing", "Test d'intégration", "통합 시험"]},
 {"key": "ANOVA测试", "value": ["Gage R&amp;R", "ANOVA gauge R&amp;R"]}], "translation": ["test"], "errorCode": 0}"""
-s = json.loads(s)
-print(s)
 def show_result(s):
     error_code = s["errorCode"]
     if error_code == 0:
-        print(s["basic"])
+        print(s["translation"])
+        if "basic" in s:
+            print(s["basic"])
         # click.secho('Hello %s!' % "test", fg='red', underline=True)
     elif error_code == 20:
         print('too long text(要翻译的文本过长)')
@@ -24,23 +23,17 @@ def show_result(s):
     else:
         print("no result(无词典结果)")
 
-show_result(s)
-# @click.command()
-# @click.option('--count', default=1, help='Number of greetings.')
-# @click.option('--name', prompt='Your name', help='The person to greet.')
-# def hello(count, name):
-#     """Simple program that greets NAME for a total of COUNT times."""
-#     for x in range(count):
-#         click.echo('Hello %s!' % name)
-
-# if __name__ == '__main__':
-#     hello()
-
 def get_response_json(query):
     params = {"q": query}
     response = requests.get(API_URL, params=params)
     data = response.json()
+    print(data)
     return data
+
+# s = json.loads(s)
+# print(s)
+# show_result(s)
+
 
 @click.command()
 @click.argument('words', nargs=-1)
